eed53b181931ddac657f40f5a3acb0972be7a2ec
Pathik Modi <modipathik95@gmail.com>
2024-02-03 15:51:06 +1300

    Add optimization on query to exclude password details for retreving data

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -23,8 +23,8 @@ public class AppUser
   public string Country { get; set; }
   public List<Photo> Photos { get; set; } = new();
 
-  public int GetAge()
-  {
-    return DateOfBirth.CalculateAge();
-  }
+  // public int GetAge()
+  // {
+  //   return DateOfBirth.CalculateAge();
+  // }
 }
