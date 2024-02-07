78ed5550c36f3ed87a70d9249edefa1a6180a8aa
Pathik Modi <modipathik95@gmail.com>
2024-02-08 11:43:00 +1300

    Add client side updatechanges in editprofile page

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -48,8 +48,11 @@ export class MemberEditComponent {
   }
 
   updateMember() {
-    console.log(this.member);
-    this.toastr.success('Profile updated sucessfully');
-    this.editForm?.reset(this.member);
+    this.memberService.updateMember(this.editForm?.value).subscribe({
+      next: (_) => {
+        this.toastr.success('Profile updated sucessfully');
+        this.editForm?.reset(this.member);
+      },
+    });
   }
 }
