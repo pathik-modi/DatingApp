8617ce53180ad4fa2cbb46344cf01358c19f56c4
Pathik Modi <modipathik95@gmail.com>
2024-02-08 15:20:49 +1300

    Add API functionality for photo upload

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86596404104d8edea0c67abc2a122ac051371eca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f0a5fb7dd2642193426f96224fe10a2947a0975")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
