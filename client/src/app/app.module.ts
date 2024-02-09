7ab62ae560c9522f6f40898114afe3fcceb33ddb
Pathik Modi <modipathik95@gmail.com>
2024-02-09 16:33:46 +1300

    Add broken code to resolve

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -23,6 +23,8 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+import { TestInputComponent } from './_forms/test-input/test-input.component';
+import { TextInputComponent } from './_forms/text-input/text-input.component';
 
 @NgModule({
   declarations: [
@@ -39,6 +41,8 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     MemberCardComponent,
     MemberEditComponent,
     PhotoEditorComponent,
+    TestInputComponent,
+    TextInputComponent,
   ],
   imports: [
     BrowserModule,
