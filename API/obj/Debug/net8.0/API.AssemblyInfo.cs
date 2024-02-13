700802823158647f3dc7c83320334a89938fbe2a
Pathik Modi <modipathik95@gmail.com>
2024-02-13 14:35:02 +1300

    Add messagehub in message service in client

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78dc99d2a802dd5acd6f0485ec8080e5fb8b98f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f125f184f903a4dd771bcb9bb9b971be5d5fda20")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
