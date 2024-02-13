f125f184f903a4dd771bcb9bb9b971be5d5fda20
Pathik Modi <modipathik95@gmail.com>
2024-02-13 14:23:35 +1300

    Add message hub on api fininshed

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac169ef003fb2baa793602c8ea555deffbf00409")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78dc99d2a802dd5acd6f0485ec8080e5fb8b98f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
