f792883c83cd39398ff60e4a74454a9f341e79d0
Pathik Modi <modipathik95@gmail.com>
2024-02-15 09:01:06 +1300

    Add steps for running dev enc

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbb1de4f7bae0a703ec2b77aaca5e2ac73d1fb75")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- README.md
+++ README.md
@@ -16,4 +16,8 @@
   - git switch DevelopmentEnv
   - Change the branch to "DevelopmentEnv" as main branch is connected to the deployed version.
 - Step 2: Create an "appsettings.json" file in API folder
-- Step 3: 
+- Step 3: Paste the app settings text in the "appsettings.json" file.
+- Step 4: Open the terminal
+  - cd API : go to the API folder
+  - dotnet run : run this command to start a local host
+  - go to this url: https://localhost:5001/ 
