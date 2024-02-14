e8a46dd9899a2e096e9d30fdc1deb625d6947868
Pathik Modi <modipathik95@gmail.com>
2024-02-15 09:50:33 +1300

    Add page size change to member list and essages

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a0f5be4d64e4fa92c518a32582bf9f9decd5b5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/_models/userParams.ts
+++ client/src/app/_models/userParams.ts
@@ -5,7 +5,7 @@ export class UserParams {
   minAge = 18;
   maxAge = 99;
   pageNumber = 1;
-  pageSize = 5;
+  pageSize = 10;
 
   constructor(user: User) {
     this.gender = user.gender == 'female' ? 'male' : 'female';
